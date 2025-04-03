83ea53ee2af28bc14e8135f5fac703a0d6ffdcb5
Netaji Bandi <netaji@outlook.com>
2025-04-03 10:24:49 -0500

    add for attributes to labels in event form

--- src/routes/newevent/+page.svelte
+++ src/routes/newevent/+page.svelte
@@ -9,7 +9,7 @@
 		<legend class="fieldset-legend">Add Event</legend>
 
 		<div class="mx-auto flex max-w-xs flex-col gap-3">
-			<label class="fieldset-label">Title</label>
+			<label class="fieldset-label" for="title">Title</label>
 			<input
 				type="text"
 				class="input"
@@ -20,18 +20,17 @@
 				value={data.event?.title || ''}
 			/>
 
-			<label class="fieldset-label">Description</label>
+			<label class="fieldset-label" for="description">Description</label>
 			<textarea
 				class="textarea"
 				placeholder="Type here"
-				value={data.event?.description || ''}
 				id="description"
 				name="description"
 				rows="3"
 				required
-			/>
+			>{data.event?.description || ''}</textarea>
 
-			<label class="fieldset-label">Date</label>
+			<label class="fieldset-label" for="date">Date</label>
 			<input
 				type="datetime-local"
 				class="input"
